95eaa2d20e66c856f30f40a658063fdb8eb2df64
Grigory Entin <grigory.entin@gmail.com>
2019-01-31 22:22:54 +0100

    Fixed Swift 4.2 conversion.

--- GEDebugKit/Shaking.swift
+++ GEDebugKit/Shaking.swift
@@ -16,8 +16,8 @@ private func debugOnShake() {
 
 func configureShakeGesture() {
 	
-	typealias ImpBlock = @convention(block) (UIWindow?, UIEvent.UIEvent.EventSubtype, UIEvent?) -> Void
-	typealias Imp = @convention(c) (UIWindow?, Selector, UIEvent.UIEvent.EventSubtype, UIEvent?) -> Void
+	typealias ImpBlock = @convention(block) (UIWindow?, UIEvent.EventSubtype, UIEvent?) -> Void
+	typealias Imp = @convention(c) (UIWindow?, Selector, UIEvent.EventSubtype, UIEvent?) -> Void
 	let sel = #selector(UIWindow.motionEnded(_:with:))
 	let method = class_getInstanceMethod(UIWindow.self, sel)!
 	let oldImp = unsafeBitCast(method_getImplementation(method), to: Imp.self)
